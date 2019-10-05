10f5d476bb3b5860a1c957983f93d317a2937c55
archi <archita810@gmail.com>
2019-10-06 02:38:16 +0530

    retofit

--- app/src/main/java/com/archi/pokedex/MainActivity.java
+++ app/src/main/java/com/archi/pokedex/MainActivity.java
@@ -6,40 +6,126 @@ import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.squareup.picasso.OkHttp3Downloader;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-
+    public ArrayList<RetroPokemon>  pokemons = new ArrayList<>();
+    boolean visited[] = new boolean[200];
     int count =1;
     int TotalCount =200;
+    TextView text;
     ArrayList<PokemonFront> pokemonFronts;
-    String frontUrl = "https://pokeapi.co/api/v2/pokemon-form/";
+    //String frontUrl = "https://pokeapi.co/api/v2/pokemon-form/";
+    String baseUrl = "https://pokeapi.co/api/v2/pokemon/";
     RecyclerAdapter recyclerAdapter;
     RecyclerView recyclerView;
     ImageView image1;
+    ImageButton left,right;
+    Retrofit retrofit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView = findViewById(R.id.view);
-        pokemonFronts = new ArrayList<>();
-        call(frontUrl,count);
+        //recyclerView = findViewById(R.id.view);
+        image1 = findViewById(R.id.pokeimage);
+        text = findViewById(R.id.name);
+        left = findViewById(R.id.left);
+        right = findViewById(R.id.right);
+        left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(count>1)
+                {
+                    count--;
+                    if(visited[count]==false)
+                    {
+                        start();
+                    }else
+                    {
+                        generateDataList(pokemons.get(count-1));
+                    }
+                }
+            }
+        });
+
+        right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count++;
+                if(visited[count]==false)
+                {
+                    start();
+                }else
+                {
+                    generateDataList(pokemons.get(count-1));
+                }
+
+            }
+        });
+        if(retrofit==null)
+        {
+            retrofit = new retrofit2.Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(GsonConverterFactory.create()).build();
+        }
+        if(visited[count]!=true)
+        {
+            start();
+        }else
+        {
+            generateDataList(pokemons.get(count-1));
+        }
+
+
+        //pokemonFronts = new ArrayList<>();
+        //call(frontUrl,count);
 
     }
 
-    public void call(String url,int count)
+    private void start() {
+        GetDataService service = retrofit.create(GetDataService.class);
+        retrofit2.Call<RetroPokemon> call = service.getAllInfo(count);
+        call.enqueue(new retrofit2.Callback<RetroPokemon>() {
+            @Override
+            public void onResponse(retrofit2.Call<RetroPokemon> call, retrofit2.Response<RetroPokemon> response) {
+                generateDataList(response.body());
+            }
+
+            @Override
+            public void onFailure(retrofit2.Call<RetroPokemon> call, Throwable t) {
+
+            }
+        });
+    }
+
+    private void generateDataList(RetroPokemon body) {
+        Picasso.Builder builder = new Picasso.Builder(getApplicationContext());
+        builder.downloader(new OkHttp3Downloader(getApplicationContext()));
+        builder.build().load(body.getSprites().getFront_default()).into(image1);
+        text.setText(body.getName());
+    }
+
+/*    public void call(String url,int count)
     {
         NetworkCall(url+count+"/");
     }
@@ -85,10 +171,10 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
-    }
+    }*/
 
 
-    public void parseJson(String s)
+    /*public void parseJson(String s)
     {
         try {
             JSONObject root = new JSONObject(s);
@@ -103,5 +189,5 @@ public class MainActivity extends AppCompatActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
-    }
+    }*/
 }
---
+++ app/src/main/java/com/archi/pokedex/RetroPokemon.java
@@ -0,0 +1,216 @@
+package com.archi.pokedex;
+
+public class RetroPokemon {
+
+        //private Location_area_encounters[] location_area_encounters;
+
+        //private Types[] types;
+
+        private String base_experience;
+
+        //private Held_items[] held_items;
+
+        private String weight;
+
+        private String is_default;
+
+        private Sprites sprites;
+
+        //private Abilities[] abilities;
+
+        //private Game_indices[] game_indices;
+
+        //private Species species;
+
+        //private Stats[] stats;
+
+        //private Moves[] moves;
+
+        private String name;
+
+        private String id;
+
+        //private Forms[] forms;
+
+        private String height;
+
+        private String order;
+
+        /*public Location_area_encounters[] getLocation_area_encounters ()
+        {
+            return location_area_encounters;
+        }
+
+        public void setLocation_area_encounters (Location_area_encounters[] location_area_encounters)
+        {
+            this.location_area_encounters = location_area_encounters;
+        }
+
+        public Types[] getTypes ()
+        {
+            return types;
+        }
+
+        public void setTypes (Types[] types)
+        {
+            this.types = types;
+        }*/
+
+        public String getBase_experience ()
+        {
+            return base_experience;
+        }
+
+        public void setBase_experience (String base_experience)
+        {
+            this.base_experience = base_experience;
+        }
+
+        /*public Held_items[] getHeld_items ()
+        {
+            return held_items;
+        }
+
+        public void setHeld_items (Held_items[] held_items)
+        {
+            this.held_items = held_items;
+        }*/
+
+        public String getWeight ()
+        {
+            return weight;
+        }
+
+        public void setWeight (String weight)
+        {
+            this.weight = weight;
+        }
+
+        public String getIs_default ()
+        {
+            return is_default;
+        }
+
+        public void setIs_default (String is_default)
+        {
+            this.is_default = is_default;
+        }
+
+        public Sprites getSprites ()
+        {
+            return sprites;
+        }
+
+        public void setSprites (Sprites sprites)
+        {
+            this.sprites = sprites;
+        }
+
+        /*public Abilities[] getAbilities ()
+        {
+            return abilities;
+        }
+
+        public void setAbilities (Abilities[] abilities)
+        {
+            this.abilities = abilities;
+        }
+
+        public Game_indices[] getGame_indices ()
+        {
+            return game_indices;
+        }
+
+        public void setGame_indices (Game_indices[] game_indices)
+        {
+            this.game_indices = game_indices;
+        }
+
+        public Species getSpecies ()
+        {
+            return species;
+        }
+
+        public void setSpecies (Species species)
+        {
+            this.species = species;
+        }
+
+        public Stats[] getStats ()
+        {
+            return stats;
+        }
+
+        public void setStats (Stats[] stats)
+        {
+            this.stats = stats;
+        }
+
+        public Moves[] getMoves ()
+        {
+            return moves;
+        }
+
+        public void setMoves (Moves[] moves)
+        {
+            this.moves = moves;
+        }*/
+
+        public String getName ()
+        {
+            return name;
+        }
+
+        public void setName (String name)
+        {
+            this.name = name;
+        }
+
+        public String getId ()
+        {
+            return id;
+        }
+
+        public void setId (String id)
+        {
+            this.id = id;
+        }
+
+        /*public Forms[] getForms ()
+        {
+            return forms;
+        }
+
+        public void setForms (Forms[] forms)
+        {
+            this.forms = forms;
+        }
+
+        public String getHeight ()
+        {
+            return height;
+        }*/
+
+        public void setHeight (String height)
+        {
+            this.height = height;
+        }
+
+        public String getOrder ()
+        {
+            return order;
+        }
+
+        public void setOrder (String order)
+        {
+            this.order = order;
+        }
+
+        /*@Override
+        public String toString()
+        {
+            return "ClassPojo [location_area_encounters = "+location_area_encounters+", types = "+types+", base_experience = "+base_experience+", held_items = "+held_items+", weight = "+weight+", is_default = "+is_default+", sprites = "+sprites+", abilities = "+abilities+", game_indices = "+game_indices+", species = "+species+", stats = "+stats+", moves = "+moves+", name = "+name+", id = "+id+", forms = "+forms+", height = "+height+", order = "+order+"]";
+        }*/
+    }
+
+
